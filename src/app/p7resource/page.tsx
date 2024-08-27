9641e67e8430a6576bfc49acdedc5ef797eb982b
pankajkumarsrm <adityagupta4962@gmail.com>
2024-08-28 01:12:05 +0530

    edit p7resource id page

--- src/app/p7resource/page.tsx
+++ src/app/p7resource/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { ResourceCardProps, Resource } from "../data/resources";
 import Heading7 from "@/components/p7resource/heading7/Heading7";
+
 const ResourceList: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 12; // Number of items per page
@@ -34,23 +35,27 @@ const ResourceList: React.FC = () => {
       <Heading7 />
       <div className="mt-6 max-w-[90%] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-1 py-3 -black ml-20 ">
         {Resource.map((item) => (
-          <div key={item.id} className="flex  bg-white p-4 flex-shrink-0  ">
-            <div className="bg-white border rounded-lg py-4 px-2 shadow-md  w-auto ">
-              <div className="flex justify-center mb-2">
+          <div key={item.id} className="flex  bg-white p-4 flex-shrink-0 ">
+            <div className="relative bg-white border rounded-lg p-5  shadow-md  w-auto ">
+              <div className=" flex mb-2">
                 <img
                   src={item.logoSrc}
                   alt={`${item.title} logo`}
                   className="w-full h-auto"
                 />
-                <div className="absolute mt-[13.2rem]  ml-[-16rem] text-base h-6 w-24 bg-green-700 text-white font-semibold ">
+                
+              </div>
+              <div className="absolute mt-[-2rem] bg-green-700 text-white font-semibold ">
                   <h3>{item.date}</h3>
                 </div>
-              </div>
 
               <div className="text-sm mb-2">
-                <span className="text-black-500"> {item.title}</span>
+                <span className="text-black-500 font-semibold"> {item.title}</span>
               </div>
-              <div className="text-gray-500 text-sm text-justify"> {item.discription} </div>
+              <div className="text-gray-500 text-sm text-justify">
+                {" "}
+                {item.discription}{" "}
+              </div>
 
               <Link href={`/p7resource/${item.id}`}>
                 <button className="h-8 mt-2 bg-green-700 text-white py-1 px-4 rounded-lg text-sm">
@@ -74,7 +79,7 @@ const ResourceList: React.FC = () => {
         <span className="text-green-700">
           Page {currentPage} of {totalPages}
         </span>
-
+         
         <button
           onClick={handleNextPage}
           className="px-2 py-1 ml-2 bg-green-600 text-white rounded-lg"
@@ -82,6 +87,7 @@ const ResourceList: React.FC = () => {
         >
           Next
         </button>
+       
       </div>
     </div>
   );

91c8f7d662c81680ef45c29d132c6b50acb89c52
Akshatsharma-89 <akshat.sharma@ascorptechnologies.com>
2024-08-30 03:18:58 +0530

    madified landing page

--- src/components/web/signup/Signup.tsx
+++ src/components/web/signup/Signup.tsx
@@ -9,7 +9,7 @@ const Signup = () => {
     alert("Form submitted!");
   };
   return (
-    <div className="flex flex-col md:flex-row items-center md:  justify-between p-2 m-3 gap-5 bg-gray-100 border-2 border-grey ">
+    <div className="flex  items-center  justify-between p-2 m-3 gap-5 bg-gray-100 border-2 border-grey ">
       {/* Left Image Container */}
       <div className="flex-1 flex items-center justify-center  md:mb-0">
         <Image
@@ -22,14 +22,14 @@ const Signup = () => {
       </div>
 
       {/* Middle Content Container */}
-      <div className=" m-2  justify-center text-center w-[33%] md:text-left  ">
+      <div className=" m-2  justify-center text-center w-[33%]  ">
         <div>
-          <p className="text-green-700 text-lg font-semibold mb-3">
+          <p className="text-green-700 text-lg font-semibold  mb-3">
             Your smooth salling starts from here
           </p>
-          <p className="text-sm text-gray-700 mb-3 ">
+          <p className="text-sm tracking-normal text-gray-700 mb-3 ">
             <span className="mt-3">
-              Create an account or sign in, By contiuing,you agree to our
+              Create an account or sign in, By contiuing, you agree to our
             </span>{" "}
             <span className="text-green-700">Terms of use</span> and acknowledge
             Our <span className="text-green-700">Privacy Policy.</span>
@@ -41,19 +41,13 @@ const Signup = () => {
             type="button"
             className="text-black bg-white border border-green-800 hover:bg-green-600 focus:ring-1 focus:outline-none focus:ring-green-8 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-green-800 me-2 mb-2"
           >
-            <svg
-              className="w-4 h-4 me-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 18 19"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <Image
+              src="/google.png"
+              alt="My Styled Image"
+              width={800}
+              height={800}
+              className="w-5 h-5 me-2"
+            />{" "}
             Sign in with Google
           </button>
 
@@ -61,19 +55,13 @@ const Signup = () => {
             type="button"
             className="text-black bg-white hover:bg-green-600 focus:ring-1 focus:outline-none focus:ring-green-8 font-medium border border-green-800 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-green-800 me-2 mb-2"
           >
-            <svg
-              className="w-4 h-4 me-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 8 19"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <Image
+              src="/facebook.png"
+              alt="My Styled Image"
+              width={800}
+              height={800}
+              className="w-5 h-5 me-2"
+            />{" "}
             Sign in with Facebook
           </button>
 
@@ -112,8 +100,12 @@ const Signup = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-center  text-white rounded-lg shadow-md hover:bg-gray-900"
-              ><img src="./play.png" alt="image not found" className="w-40 h-[2rem]]"/>
-               
+              >
+                <img
+                  src="./play.png"
+                  alt="image not found"
+                  className="w-40 h-[2rem]]"
+                />
               </a>
 
               {/* App Store Button */}
@@ -123,10 +115,11 @@ const Signup = () => {
                 rel="noopener noreferrer"
                 className="flex items-center mr-2  text-white rounded-lg shadow-md hover:bg-gray-800"
               >
-                <img src="./apple.png" alt="image not found" className="w-40 h-[3rem]"/>
-                  
-                
-                
+                <img
+                  src="./apple.png"
+                  alt="image not found"
+                  className="w-40 h-[3rem]"
+                />
               </a>
             </div>
           </div>

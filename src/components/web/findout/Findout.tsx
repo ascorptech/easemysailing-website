4aeb5e92d125a4605cf47707bd4fd19519937458
Akshatsharma-89 <akshat.sharma@ascorptechnologies.com>
2024-09-18 19:09:27 +0530

    new layout

--- src/components/web/findout/Findout.tsx
+++ src/components/web/findout/Findout.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 const Findout = () => {
   return (
     <div className="flex items-center    bg-[url('/ocean.png')] ">
-      <div className="flex  bg-[#021527] bg-opacity-65 justify-between w-full items-center p-16 gap-10 ">
+      <div className="flex  bg-[#021527] bg-opacity-65 justify-between w-full items-center p-16 gap-8 ">
         {/* Left div */}
         <div className="w-[50%]">
           <div className="relative h-[400px]  ">
@@ -22,11 +24,19 @@ const Findout = () => {
             FIND OUT WHAT'S NEW A{" "}
             <span className="text-[#00A264]"> EASEMYSAILING </span>
           </h1>
-          <p className="text-white mt-8 text-[16px]">
+          <p className="text-white mt-5 text-[16px]">
             Your career journey is serious, but should never be lonely or dull.
             With the new EaseMySailing, work communities are right at your
             fingertips.
           </p>
+          <div className="mt-[7rem]">
+            <Link
+              href="#"
+              className="bg-[#00A264] px-1 flex justify-center items-center text-white lg:px-2 lg:h-[50px] rounded-full text-base w-[160px]"
+            >
+              Contact Us
+            </Link>
+          </div>
         </div>
       </div>
     </div>

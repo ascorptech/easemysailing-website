853aa1bd7d7fc2bb1b129489fecb25980580d883
Akshatsharma-89 <akshat.sharma@ascorptechnologies.com>
2024-09-25 14:28:26 +0530

    latest

--- src/components/web/companies/Slide.tsx
+++ src/components/web/companies/Slide.tsx
@@ -57,13 +57,13 @@ const Slide = () => {
 
   const scrollLeft = () => {
     if (card.current) {
-      card.current.scrollBy({ left: -200, behavior: "smooth" });
+      card.current.scrollBy({ left: -147, behavior: "smooth" });
     }
   };
 
   const scrollRight = () => {
     if (card.current) {
-      card.current.scrollBy({ left: 200, behavior: "smooth" });
+      card.current.scrollBy({ left: 147, behavior: "smooth" });
     }
   };
 
@@ -83,24 +83,24 @@ const Slide = () => {
   }, []);
 
   return (
-    <div className=" lg:ml-[0rem]  relative flex items-center justify-center w-[100%] lg:w-[1328px] lg:h-[200px]  ">
+    <div className=" lg:ml-[0rem]  relative flex items-center justify-center w-[80%] lg:w-[1435px] lg:h-[200px]  ">
       {/* Left Arrow */}
       <button
         onClick={scrollLeft}
         className="absolute left-1 z-10 border-2 w-[23.6px] h-[23.6px]  rounded-full border-black ml-1  mr-2"
       >
-        <MdArrowBack className="ml-[2px]"/>
+        <MdArrowBack className=""/>
       </button>
 
       {/* Carousel Wrapper */}
       <div
         ref={card}
-        className=" lg:mr-12   lg:gap-0 gap-3 flex overflow-x-scroll mt-3 no-scrollbar scroll-smooth snap-x mx-[4rem] lg:mx-0 snap-mandatory lg:ml-[2.4rem] lg:w-[1266px] lg:h-[100px]"
+        className=" lg:mr-12 w-36  lg:gap-1 gap-3 flex overflow-x-scroll lg:mt-3  mt-3 no-scrollbar scroll-smooth snap-x mx-[4rem] lg:mx-0 snap-mandatory lg:ml-[2.4rem] lg:w-[1276px] lg:h-[100px]"
       >
         {cards.map((card) => (
           <div
             key={card.id}
-            className="flex lg:mb-2 h-[12rem] lg:px-1 lg:w-[250px] lg:h-[84px] sm:w-[16.9%] "
+            className="flex ml-[6px] lg:ml-0 lg:mb-2 h-[12rem] lg:px-1 lg:w-[250px] lg:h-[84px] sm:w-[16.9%] "
           >
             <div className="flex flex-col items-center justify-center bg-white rounded-lg border-2 shadow-md p-6 w-[129.8px] ">
               <div className="   px-2 ">

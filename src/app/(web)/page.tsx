021a076bd3f8a8d06946a0999184deaa8ad28a97
Shikha manga <shikhamanga@Shikhas-Laptop.local>
2024-09-28 19:38:24 +0530

    fix the client slider

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       <Navigation />
       <Sailors />
       <PartnersSection />
-      {/* <ClientSection /> */}
+      <ClientSection />
       <Aimergee />
       {/* <Status/> */}
 

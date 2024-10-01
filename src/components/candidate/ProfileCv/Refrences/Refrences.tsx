9b46227bf3d1b84bc9d779d4b0226f838f8493a6
pankajkumarsrm <adityagupta4962@gmail.com>
2024-10-02 00:26:48 +0530

    latest

--- src/components/candidate/ProfileCv/Refrences/Refrences.tsx
+++ src/components/candidate/ProfileCv/Refrences/Refrences.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 
 import { useState } from "react";
 
@@ -8,20 +8,22 @@ type RefrencesComplete = {
   percentage: number;
   color: string;
 };
-type Props={
-  additionalComplete: RefrencesComplete ; 
-  setAdditionalComplete: React.Dispatch<React.SetStateAction<RefrencesComplete >>;
-  userDetail:any
-}
+type Props = {
+  refrencesComplete: RefrencesComplete;
+  setRefrencesComplete: React.Dispatch<React.SetStateAction<RefrencesComplete>>;
+  userDetail: any;
+};
 
-const Refrences = () => {
+const Refrences = ({
+  refrencesComplete,
+  setRefrencesComplete,
+  userDetail,
+}: Props) => {
   const [date, setDate] = useState("");
   const [companyName, setCompanyName] = useState("");
+  const [companyName1, setCompanyName1] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-
-
-
   const [addInfo, setAddInfo] = useState("");
   const [postalCode, setPostalCode] = useState("");
   const [state, setState] = useState("");
@@ -29,42 +31,108 @@ const Refrences = () => {
   const [email, setEmail] = useState("");
   const [indNumber, setIndNumber] = useState("");
   const [cityName, setCityName] = useState("");
-  
+  const [issued, setIssued] = useState("");
+  const [document1, setDocument1] = useState("");
+  const [issuingCountry, setIssuingCountry] = useState("");
+  const [countryCode, setCountryCode] = useState("");
+  const [country, setCountry] = useState("");
+  const [ nearestAirport, setNearestAirport] = useState("");
+  const [ countryCode1, setCountryCode1] = useState("");
 
-  
+
+
+
+  const totalFields = 19;
+  const filledFields = [
+    date,
+    companyName,
+    companyName1,
+    firstName,
+    lastName,
+    addInfo,
+    postalCode,
+    state,
+    phoneNumber,
+    email,
+    indNumber,
+    cityName,
+    issued,
+    document1,
+    issuingCountry,
+    countryCode,
+    country,
+    nearestAirport,
+     countryCode1
+  ].filter(Boolean).length;
+
+  const percentage = (filledFields / totalFields) * 100;
+
+  let color;
+  useEffect(() => {
+    console.log("user", userDetail);
+    if (percentage <= 30) {
+      setRefrencesComplete((prevState) => ({
+        ...prevState,
+        percentage: percentage,
+        color: "#FF0000",
+      }));
+      color = "red";
+    } else if (percentage <= 70) {
+      setRefrencesComplete((prevState) => ({
+        ...prevState,
+        percentage: percentage,
+        color: "#FF9900",
+      }));
+      color = "#FF9900";
+    } else {
+      setRefrencesComplete((prevState) => ({
+        ...prevState,
+        percentage: percentage,
+        color: "#00A264",
+      }));
+      color = "green";
+    }
+  }, [percentage, color]);
 
   return (
     <div className=" container border-2 shadow-lg p-3  mt-[14px] mb-8 ">
       <div className="">
-        <div className="text-center my-4"><h1 className="font-bold">EVALUATION REPORT AND REFERENCE/RECOMMENDATION LETTERS</h1></div>
+        <div className="text-center my-4">
+          <h1 className="font-bold">
+            EVALUATION REPORT AND REFERENCE/RECOMMENDATION LETTERS
+          </h1>
+        </div>
         <div className=" flex flex-col  px-4  gap-4">
           <div className="flex items-center justify-between gap-2  ">
-            
             <div className=" w-[25%]">
               <label
                 className="block text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-1"
-                htmlFor="street"
+                htmlFor="issued"
               >
                 Issued
               </label>
-              <select className="border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]">
-                <option value="maile">2</option>
-                <option value="maile">5</option>
-                <option value="maile">8</option>
+              <select
+                id="issued"
+                className="border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]"
+                value={issued}
+                onChange={(e) => setIssued(e.target.value)}
+              >
+                <option value="maile1">2</option>
+                <option value="maile2">5</option>
+                <option value="maile3">8</option>
               </select>
             </div>
-
 
             <div className="w-[25%] ">
               <label
                 className="block text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-1"
-                htmlFor="number"
+                htmlFor="issuedate1"
               >
                 Issue Date
               </label>
               <div className="relative flex items-center  ">
                 <input
-                  id="number"
+                  id="issuedate1"
                   type="date"
                   value={date}
                   onChange={(e) => setDate(e.target.value)}
@@ -78,13 +146,13 @@ const Refrences = () => {
             <div className="w-[25%] ">
               <label
                 className="block text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-1"
-                htmlFor="street"
+                htmlFor="vessel"
               >
                 Vessel/Company Name
               </label>
               <div className="relative flex items-center  ">
                 <input
-                  id="street"
+                  id="vessel"
                   type="text"
                   value={companyName}
                   onChange={(e) => setCompanyName(e.target.value)}
@@ -97,16 +165,16 @@ const Refrences = () => {
             <div className="w-[25%] ">
               <label
                 className="block text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-1"
-                htmlFor="street"
+                htmlFor="document1"
               >
                 Document
               </label>
               <div className="relative flex items-center  ">
                 <input
-                  id="street"
+                  id="document1"
                   type="text"
-                  value={companyName}
-                  onChange={(e) => setCompanyName(e.target.value)}
+                  value={document1}
+                  onChange={(e) => setDocument1(e.target.value)}
                   className="border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]"
                   placeholder=""
                   required
@@ -117,21 +185,19 @@ const Refrences = () => {
 
           <h1 className="text-center font-bold my-4">CRIMINAL RECORD</h1>
 
-
-
           <div className="flex items-center justify-between gap-2  ">
-            
             <div className="flex flex-col w-full  ">
-              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 ">
+              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 " htmlFor="issuingcountry1">
                 Issuing Country
               </label>
-              <select className=" border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]">
-                <option value="maile">India</option>
-                <option value="maile">Austrelia</option>
-                <option value="maile">England</option>
+              <select id="issuingcountry1" className=" border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]" 
+              value={issuingCountry}
+              onChange={(e) => setIssuingCountry(e.target.value)} >
+                <option value="india">India</option>
+                <option value="Austrelia">Austrelia</option>
+                <option value="england">England</option>
               </select>
             </div>
-
 
             <div className="w-full">
               <label
@@ -176,11 +242,8 @@ const Refrences = () => {
 
           <h1 className="text-center font-bold my-4">REFERENCE CONTACTS</h1>
 
-
-
-
-        {/* third section start*/}
-        <div className="flex items-center justify-between gap-2  ">
+          {/* third section start*/}
+          <div className="flex items-center justify-between gap-2  ">
             {" "}
             <div className="w-[25%] ">
               <label
@@ -202,68 +265,57 @@ const Refrences = () => {
               </div>
             </div>
             <div className="w-[25%] ">
-            <label
-              className="block text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-1"
-              htmlFor="lastName"
-            >
-              Last Name
-            </label>
-            <div className="relative flex items-center  ">
-              <input
-                id="lastName"
-                type="text"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                className="border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]"
-                placeholder=""
-                required
-              />
-            </div>
-          </div>
-
-          <div className="w-[25%] ">
               <label
                 className="block text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-1"
-                htmlFor="street"
+                htmlFor="lastName"
               >
-                Vessel/Company Name
+                Last Name
               </label>
               <div className="relative flex items-center  ">
                 <input
-                  id="street"
+                  id="lastName"
                   type="text"
-                  value={companyName}
-                  onChange={(e) => setCompanyName(e.target.value)}
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
                   className="border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]"
                   placeholder=""
                   required
                 />
               </div>
             </div>
-
-            <div className="flex flex-col w-[25%]  ">
-              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 ">
-               Country Code
+            <div className="w-[25%] ">
+              <label
+                className="block text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-1"
+                htmlFor="street1"
+              >
+                Vessel/Company Name
               </label>
-              <select className=" border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]">
-                <option value="maile">45440</option>
-                <option value="maile">02458</option>
-                <option value="maile">010203</option>
+              <div className="relative flex items-center  ">
+                <input
+                  id="street1"
+                  type="text"
+                  value={companyName1}
+                  onChange={(e) => setCompanyName1(e.target.value)}
+                  className="border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]"
+                  placeholder=""
+                  required
+                />
+              </div>
+            </div>
+            <div className="flex flex-col w-[25%]  ">
+              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 " htmlFor="contryCode">
+                Country Code
+              </label>
+              <select id="countryCode" className=" border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]" value={countryCode}
+                  onChange={(e) => setCountryCode(e.target.value)} >
+                <option value="45440">45440</option>
+                <option value="02458">02458</option>
+                <option value="02453">010203</option>
               </select>
             </div>
-
-
-         
-
-           
-          
           </div>
 
-
-        {/* third section end */}
-
-
-
+          {/* third section end */}
 
           <div className=" ">
             <label
@@ -287,12 +339,12 @@ const Refrences = () => {
 
           <div className="flex items-center gap-4">
             <div className="w-[50%]">
-              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 ">
+              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 " htmlFor="postalcode">
                 Postal Code/ZIP Code
               </label>
               <div className="relative flex items-center  ">
                 <input
-                  id="addinfo"
+                  id="postalcode"
                   type="number"
                   value={postalCode}
                   onChange={(e) => setPostalCode(e.target.value)}
@@ -305,13 +357,13 @@ const Refrences = () => {
             <div className="w-[50%] ">
               <label
                 className="blocktext-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-1"
-                htmlFor="cityName"
+                htmlFor="cityName1"
               >
                 City
               </label>
               <div className="relative flex items-center  ">
                 <input
-                  id="cityName"
+                  id="cityName1"
                   type="text"
                   value={cityName}
                   onChange={(e) => setCityName(e.target.value)}
@@ -346,13 +398,15 @@ const Refrences = () => {
               </div>
             </div>
             <div className="flex flex-col w-[50%]  ">
-              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 ">
+              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 " htmlFor="country">
                 Country
               </label>{" "}
-              <select className=" border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]">
-                <option value="maile">India</option>
-                <option value="maile">Austrelia</option>
-                <option value="maile">England</option>
+              <select id="country" className=" border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}>
+                <option value="maile1">India</option>
+                <option value="maile2">Austrelia</option>
+                <option value="maile3">England</option>
               </select>
             </div>
           </div>
@@ -363,10 +417,12 @@ const Refrences = () => {
             <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 ">
               Nearest Airport
             </label>
-            <select className=" border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]">
-              <option value="maile">India</option>
-              <option value="maile">Austrelia</option>
-              <option value="maile">England</option>
+            <select id="NearestAirport" className=" border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]"   
+            value={nearestAirport}
+                  onChange={(e) => setNearestAirport(e.target.value)}>
+              <option value="maile1">India</option>
+              <option value="maile2">Austrelia</option>
+              <option value="maile3">England</option>
             </select>
           </div>
 
@@ -374,13 +430,14 @@ const Refrences = () => {
 
           <div className="flex items-center justify-between gap-4  ">
             <div className="flex flex-col  ">
-              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 ">
+              <label className="text-[14px] leading-[19.07px] font-[openSans] text-[#333333] mb-2 " htmlFor="countryCode1">
                 Country Code
               </label>
-              <select className="border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]">
-                <option value="maile">+91</option>
-                <option value="maile">+50</option>
-                <option value="maile">+08</option>
+              <select id="countryCode1" className="border rounded-md w-full h-9  px-2  text-[14px] leading-[19.07px] font-[openSans] text-[#333333] focus:outline-[#00A264] focus:shadow-outline border-[#00A264]"   value={countryCode1}
+                  onChange={(e) => setCountryCode1(e.target.value)}>
+                <option value="maile1">+91</option>
+                <option value="maile2">+50</option>
+                <option value="maile3">+08</option>
               </select>
             </div>
 

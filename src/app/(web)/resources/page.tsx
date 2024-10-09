a215aebdaa0a821e153c242cee16e22140fdeaed
Vinay Sharma <vinaysharma.7503@gmail.com>
2024-10-09 18:51:43 +0530

    latest code

--- src/app/(web)/resources/page.tsx
+++ src/app/(web)/resources/page.tsx
@@ -7,7 +7,11 @@ const page = () => {
   return (
     <div className="flex flex-col mb-4">
       <Heading7 />
-      
+      <div className="my-5 flex text-center lg:justify-center tracking-wider items-center ">
+        <p className="text-left">
+        Disclaimer: The views expressed in these articles are the personal opinion of the writer
+        </p>
+      </div>
       <ResourcesList/>
     </div>
   );
